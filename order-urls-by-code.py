48c7c6a9ffd9e49c7ae578af59bd4428f64af1a0
C.S. by G.B <96747355+CSbyGB@users.noreply.github.com>
2022-07-22 18:16:06 -0400

    Added more error codes

--- order-urls-by-code.py
+++ order-urls-by-code.py
@@ -2,21 +2,36 @@
 
 import requests
 import sys
+import urllib3
+
+# Remove warnings when using verify=False
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # We take the list of urls from the argument
 urlFile = sys.argv[1]
 
 # Create file to put all our 400 urls
 f400 = open("400-codes.txt", "w")
-# x = requests.get("https://google.com/")
-# print(x.status_code)
+# Create file to put all our 200 urls
+f200 = open("200-codes.txt", "w")
+# Create file to put all our 300 urls
+f300 = open("300-codes.txt", "w")
+# response = requests.get("https://google.com/")
+# print(response.status_code)
+ferror = open("error.txt", "w")
 
 with open (urlFile) as f:
 	for line in f:
 		print(line)
 		print("getting: "+line.strip())	
 		# use verify=False with caution
-		x = requests.get(line.strip(), verify=False)
-		if (x.status_code >= 400) and (x.status_code < 500):
-			f400.write("\nurl: "+line+"gives code: "+str(x.status_code))
-
+		try:
+			response = requests.get(line.strip(), verify=False)
+			if (response.status_code >= 400) and (response.status_code < 500):
+				f400.write("\nurl: "+line.strip()+" gives code: "+str(response.status_code))
+			if (response.status_code >= 200) and (response.status_code < 300):
+				f200.write("\nurl: "+line.strip()+" gives code: "+str(response.status_code))
+			if (response.status_code >= 300) and (response.status_code < 400):
+				f300.write("\nurl: "+line.strip()+" gives code: "+str(response.status_code)+" redirects to "+response.geturl())		
+		except requests.exceptions.RequestException as error:
+			ferror.write("\nurl: "+line.strip()+" Error: "+str(error))
